7da4a890c4bfeb29edef6fea73bc014418db99fe
jerald macachor <jerald.macachor@gmail.com>
2021-04-04 23:24:34 -0700

    add color background

--- js/app.js
+++ js/app.js
@@ -1,36 +1,82 @@
-// get element button id= btnSave
-// add event listener click to btnSave, saveTodo 
-//    get element input[text] id= todoText
-//    get element div id= todoListDisplay
-//    get text from todoText
-//    create element todo - 
-//    add element todo to todoListDisplay 
+let todoInput;
+let pendingTodos;
+
+function* getTodoColorClassStyles() {
+  const todoColorClassStyles = [
+    "postit-pink",
+    "postit-skyblue",
+    "postit-yellow",
+    "postit-green",
+    "postit-purple"
+  ];
+  while (true) {
+    shuffleArray(todoColorClassStyles);
+    for (const bgStyle of todoColorClassStyles) {
+      yield bgStyle;
+    }
+  }
+}
+const colorGenerator = getTodoColorClassStyles();
+
+window.onload = init;
+
+function init() {
+  console.log('Todo App');
+  
+  todoInput = $('#todoText');
+  pendingTodos = $('#pendingTodos');
+  
+  todoInput.keypress(function (e) {
+    const enterPressed = e.key === 'Enter';
+    if (enterPressed) saveTodo();
+  });
+  
+  const btnSave = $('#btnSave');
+  btnSave.click(saveTodo);
+}
 
 function saveTodo() {
-  const todoInput = $('#todoText')
   const todoText = todoInput.val();
-  if (todoText === "") return;
-  console.log("saving Todo");
-  const todo = `
-    <div class="card todo-card">
-      <h6>${todoText}</h6>
-    </div>
-  `;
-  console.log("todo", todo);
-  const pendingTodos = $('#pendingTodos');
+  const noTodo = todoText === "";
+  if (noTodo) return;
+
+  console.log("saveTodo", todoText);
+  const todo = createTodo(todoText);
+
   pendingTodos.append(todo);
   todoInput.val("").focus();
 }
 
-function init() {
-  console.log('init');
-  const btnSave = $('#btnSave');
-  btnSave.click(saveTodo);
-  const todoText = $('#todoText');
-  todoText.keypress(function(e) {
-    console.log(e);
-  });
-
+function createTodo(todoText) {
+  const bgSytleClass = colorGenerator.next().value;
+  const escapedTodoText = escapeHtml(todoText);
+  const todoCard = `
+    <div class="card todo-card ${bgSytleClass}">
+      <h5>${escapedTodoText}</h5>
+    </div>
+  `;
+  console.log("createTodo", todoCard);
+  return todoCard;
 }
 
-window.onload = init;
+// Utility
+// Fucntion to shuffle the array content
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+      // Generate random number 
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+  }
+}
+
+function escapeHtml(unsafe) {
+  return unsafe
+       .replace(/&/g, "&amp;")
+       .replace(/</g, "&lt;")
+       .replace(/>/g, "&gt;")
+       .replace(/"/g, "&quot;")
+       .replace(/'/g, "&#039;");
+}
+// <img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/baby-yoda-old-yoda-1574103229.jpg?crop=0.486xw:0.973xh;0.514xw,0&resize=480:*">
